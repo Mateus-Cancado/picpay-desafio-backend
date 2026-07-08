6a7c0d3d725a88c3d7dee51eb3dedce2e8ab7fb2
Mateus Cançado <mateuslimacancado55@gmail.com>
2026-07-07 21:09:12 -0300

    create NotificationService, NotificationDTO, AppConfig

---
+++ src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -0,0 +1,30 @@
+package com.picpaysimplificado.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.NotificationDTO;
+
+@Service
+public class NotificationService {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	public void sendNotification(User user, String msg) throws Exception {
+		
+		String email = user.getEmail();
+		NotificationDTO notificationRequest = new NotificationDTO(email, msg);
+		
+		ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+		
+		if(!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+			System.out.println("Erro ao enviar notificação.");
+			throw new Exception("Serviço de notificação fora do ar.");
+		}
+	}
+}
